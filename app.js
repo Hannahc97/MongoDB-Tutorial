a4855f9f9764df0fcaf9d226b9ca50465933a5cf
“hxc338” <hxc338@student.bham.ac.uk>
2025-06-04 16:19:33 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+// to start type "nodemon app" in terminal
+import express from "express";
+import { connectToDb, getDb } from './db.js';
+import { ObjectId } from "mongodb";
+
+const app = express();
+app.use(express.json())
+
+const PORT = 3000;
+
+// db connection - we want to connect to db straight away before api connections
+let db;
+
+connectToDb((err)=> {
+    if(!err){
+        app.listen(PORT, ()=> {
+            console.log(`The app is listening on port ${PORT}`)
+        })
+        db = getDb() // returns db connection object that we need -> this object we use in this file to interact with db so fetch save, update data
+    }
+})
+
+
+
+// routes
+app.get("/books", (req, res) => {
+
+    let books = []
+
+    db.collection("books") // collection is a function --> used to reference a specific collection in the db --> pass in whatever collection we need 
+    // db.books --> we originally used this in the shell 
+        .find() // returns a cursor toArray forEach 
+        .sort({author: 1}) // sorts in asc for author 
+        .forEach(book => { books.push(book)  // this is async as fetching batches of docs so need .then
+        })
+        .then(() => {
+            res.status(200).json(books) // sending books array
+        })
+        .catch(()=>{
+            res.status(500).json({error: "Could not fetch the documents"})
+        })
+})
+
+app.get("/books/:id", (req, res) => {
+
+    // evaluate to true if string id is valid 
+    if(ObjectId.isValid(req.params.id)){
+        db.collection("books")
+        .findOne({_id: new ObjectId(req.params.id)})
+        .then(doc => {
+            res.status(200).json(doc) // sending the document itself we get from MongoDB
+        })
+        .catch(err => {
+            res.status(500).json({error: "Could not fetch the document"})
+        })
+    } else {
+        res.status(500).json({error: "Not a valid document id"})
+    }
+})
---
+++ db.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+let dbConnection
+
+// connect to a database
+// cb is callback function -> pass in cb function as argument that we want to fire after we try to connect to the db so after the sucess of that connection attempt or after an error 
+export const connectToDb = (cb) => {
+    // "connect" connects to the db using the connection string
+    MongoClient.connect("mongodb://localhost:27017/bookstore") // async task
+    // returns a promise that we tack a "then" method onto, to fire a function when complete
+        .then((client) => {
+            // client is what we've created by connecting the db 
+            dbConnection = client.db()
+            // on client object is method called db which returns the db connection or interface so we can interact with db so we store it into a variable 
+            cb()
+        })
+        // catch any errors if we try to connect
+        .catch(err => {
+            console.log(err)
+            cb(err)
+        })
+}
+
+// return our database connection after we've connected to it 
+export const getDb = () => dbConnection
